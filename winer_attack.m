f0ec7de5c9a19e5effe157df4a6f6079443f8bd0
Matteo Giorgi <mattegiorgi@gmail.com>
2019-09-30 14:51:14 +0200

    mathematica code

---
+++ winer_attack.m
@@ -0,0 +1,10 @@
+e = 7502876735617; n = 28562942440499; (* esempio di chiave pubblica attaccabile *)
+fc = ContinuedFraction[e/n];
+cList = Convergents[fc];
+phiList = Floor[e/Rest[cList]];
+
+check[phi_] := (x/.p) /; (Mod[n,x] /. (p=Solve[x^2-(n-phi+1)x+n==0,x])) == {0, 0};
+checkL[phi_List] := Flatten[Cases[check /@ phi,_List]];
+
+primes = Flatten[checkL /@ (phiList-m /. {m->#}& /@ Range[0,9])];
+
